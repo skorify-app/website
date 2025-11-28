f0f1d64453b93d1a3163f7731531b52f348191ce
Alldreno Hosea <Alldreno.4342401059@students.polibatam.ac.id>
2025-11-28 11:52:53 +0700

    Remove custom auth check, use built-in Auth::check() instead

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,10 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->alias([
-            'authCheck' => \App\Http\Middleware\AuthCheck::class,
-        ]);
-        $middleware->redirectGuestsTo('/');
+        //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
